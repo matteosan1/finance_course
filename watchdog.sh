9850aa108db79a52bdf05b0e95baf86e75d3ffde
Matteo Sani <matteo.sani@mpscs.it>
2019-12-03 14:30:47 +0100

    Update watchdog.sh

--- watchdog.sh
+++ watchdog.sh
@@ -6,5 +6,5 @@ echo "ASSIGNEE: $CI_MERGE_REQUEST_ASSIGNEES"
 if [[ "$GITLAB_USER_LOGIN" == "$CI_MERGE_REQUEST_ASSIGNEES" ]]
 then
     echo "An assignee cannot approve its own merge requests..."
-    exit(255)
+    exit 255
 fi
