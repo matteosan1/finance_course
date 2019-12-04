d6505e5fb15a8b2c824b92608c20064e6118ec26
Matteo Sani <matteo.sani@mpscs.it>
2019-12-04 09:55:58 +0100

    Update watchdog.py

--- watchdog.py
+++ watchdog.py
@@ -7,7 +7,7 @@ user = os.environ.get("GITLAB_USER_LOGIN")
 project = os.environ.get("CI_PROJECT_NAME")
 merge_id = os.environ.get("CI_MERGE_REQUEST_ID")
 
-data = {"project:project, "user:user, "id":merge_id}
+data = {"project":project, "user":user, "id":merge_id}
 headers = {'Content-type':'application/json', 'Accept':'application/json'}
 
 print (data)
