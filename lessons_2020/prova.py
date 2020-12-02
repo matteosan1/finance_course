d35102aa3d09c5c27963d6819fdaca716c910a55
Matteo Sani <matteosan1@gmail.com>
2020-12-02 22:20:08 +0100

    [UPD] Clean up 2

--- lessons_2020/prova.py
+++
@@ -1,9 +0,0 @@
-from datetime import date
-from dateutil.relativedelta import relativedelta
-
-def generate_swap_dates(starting_date, maturity_months):
-    dates = []
-    for i in range(0, maturity_months, 12):
-        dates.append(starting_date + relativedelta(months=i))
-    dates.append(starting_date + relativedelta(months=maturity_months))
-    return dates
