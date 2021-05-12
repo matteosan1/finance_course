aa4f3f1f79fb0c0a81b81981907088cf73e92228
Matteo Sani <matteosan1@gmail.com>
2021-05-12 21:05:11 +0200

    [FIX] Fix merge conflicts

--- hull_white_tree.py
+++ hull_white_tree.py
@@ -7,7 +7,41 @@ import math
 #t = i*deltat
 #Rs = j*deltaR
 
-def prob(a, j, deltat, type=0):
+sigma = 0.01
+a = 0.1
+deltat = 1
+deltaR = sigma*np.sqrt(3*deltat)
+jmax = math.ceil(0.184/a/deltat)
+jmin = -jmax
+
+class Node:
+    def __init__(self, i, j, type=0):
+        self.label = (i, j)
+        self.R = 0
+        self.type = None
+
+
+def setNode(n, a, deltat):
+    if n.label[1] <= jmin:
+        n = Node
+        
+    for k in range(3):
+        
+    def 
+        if type == 0:
+            self.up = (i+1, j+1)
+            self.down = (i+1, j-1)
+            self.mid = (i+1, j)
+        elif type == 1:
+            self.up = (i+1, j+2)
+            self.down = (i+1, j)
+            self.mid = (i+1, j+1)
+        elif type == -1:
+            self.up = (i+1, j)
+            self.down = (i+1, j-2)
+            self.mid = (i+1, j-1)
+
+    def prob(self, a, j, deltat):
     if type == 0:
         pu = 1/6 + 1/2*(a**2*j**2*deltat**2 - a*j*deltat)
         pm = 2/3 - a**2*j**2*deltat**2
@@ -23,10 +57,9 @@ def prob(a, j, deltat, type=0):
 
     return pu, pm, pd
 
-sigma = 0.01
-a = 0.1
-deltat = 1
-deltaR = sigma*np.sqrt(3*deltat)
-print (deltaR)
-jmax = math.ceil(0.184/a)
-print (jmax)
+
+            
+i = 0
+j = 0
+n = Node(i, j, 0)
+        
