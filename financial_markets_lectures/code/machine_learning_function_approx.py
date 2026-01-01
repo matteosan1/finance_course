ab06923d08143ad82add8424f4df779f4c310f21
Matteo Sani <matteosan1@gmail.com>
2026-01-01 22:37:34 +0100

    [UPD] Exams and lecture with 2025 material

--- financial_markets_lectures/code/machine_learning_function_approx.py
+++ financial_markets_lectures/code/machine_learning_function_approx.py
@@ -1,10 +1,11 @@
-from keras.models import Sequential
-from keras.layers import Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Input
 
 model = Sequential()
-model.add(Dense(15, input_dim=1, activation='sigmoid'))
-model.add(Dense(5, activation='sigmoid'))
-model.add(Dense(1, activation='sigmoid'))
-
+model.add(Input(shape=(1,)))
+model.add(Dense(15, activation='tanh'))
+model.add(Dense(5, activation='tanh'))
+model.add(Dense(1, activation=None))
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, epochs=5000, batch_size=10, verbose=1)
+
+history = model.fit(scaled_X, scaled_y, epochs=100, batch_size=10, verbose=1)
